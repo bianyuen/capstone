1f3ea7524ceb7571a98b03e79900f24154d9d532
Yuen <bianyuen@909c4ac4247f.ant.amazon.com>
2021-07-13 18:38:12 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+import NavBar from './NavBar';
+import Body from './Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <NavBar/>
+      <Body/>
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
