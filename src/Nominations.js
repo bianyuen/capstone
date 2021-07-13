a95900c0120f008c269c2621c577ae55cfe4d950
Yuen <bianyuen@909c4ac4247f.ant.amazon.com>
2021-07-13 21:56:27 +1000

    Added form

--- src/Nominations.js
+++ src/Nominations.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Nominations.css';
+import ReactDOM from 'react-dom';
 
 function Nominations() {
   return (
-    <div className="Nominations">
-      <h1 className = "Nominations"> Hello and welcome to community chats! </h1>
-    </div>
+      <>
+    <form>
+    <h1>Customer Nomination Form</h1>
+    <p>Customer Name: <input type="text"/> </p>
+    <p>Rep Email: <input type="text"/> </p>
+    <p>Use Case: <input type="text"/> </p>
+    <p>Business Sector: <input type="text"/> </p>
+    <p>Customer Contact: <input type="text"/> </p>
+    <br></br>
+  </form>
+
+  <button>Submit Nomination</button>
+  </>
+
+
+    
   )
 }
 
-export default Nominations
+export default Nominations;
